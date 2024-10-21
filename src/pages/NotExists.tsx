5dc12c79e96e3fe3e181880947fabbc050556872
simonecdev <simonecilurso.dev@gmail.com>
2024-10-21 22:24:36 +0200

    Mancavano loro

---
+++ src/pages/NotExists.tsx
@@ -0,0 +1,11 @@
+const NotExists = () =>  {
+
+
+    return (
+        <h1>Come sei arrivato qui ?</h1>
+    )
+
+}
+export default NotExists
+
+
---
+++ src/pages/Whoami.tsx
@@ -0,0 +1,12 @@
+
+const Whoami = () =>  {
+
+
+    return (
+        <h1>It's a me !</h1>
+    )
+
+}
+export default Whoami
+
+
